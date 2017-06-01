17fb43ed3b0397f1697b1c6c9691ee4720e878dc
Anders Linn <anderslinn@gmail.com>
2017-05-31 22:29:54 -0700

    More cleanup
    Moved the demo code to a separate function so I can start porting the
    old code over. Moved the conversion functions to CRGBW.h

--- CarpetLedLogic/CRBGW.h
+++ CarpetLedLogic/CRBGW.h
@@ -7,8 +7,6 @@
 #include "lib8tion.h"
 #include "color.h"
 
-FASTLED_NAMESPACE_BEGIN
-
 struct CRGBW : CRGB {
   union {
     uint8_t white;
@@ -575,6 +573,22 @@ struct CRGBW {
 
 */
 
-FASTLED_NAMESPACE_END
+static void convertCRGBW2CRGB( CRGBW *src, CRGB *dst ) {
+  dst[0] = CRGB(src[0].r, src[0].g, src[0].b); //(red led 1, green on led 1, blue led 1)
+  dst[1] = CRGB(src[1].g, src[0].w, src[1].r); //(green led 2, white on led 1, red led 2)
+  dst[2] = CRGB(src[1].w, src[1].b, src[2].g); //(white led 2,blue led 2, green led 3)
+  dst[3] = CRGB(src[2].b, src[2].r, src[2].w); //(blue led 3, red led 3, white led 3)
+}
+
+static void covertCRGBWArrayToCRGBArray( CRGBW *src, CRGB *dst,
+                                          uint16_t srcSize, uint16_t dstSize ) {
+  int s, d;
+  // we want to cut out if the entire array has been converted, or if we're out of leds
+  while ( s < srcSize && d < dstSize ) {
+    convertCRGBW2CRGB( src + s, dst + d );
+    s += 3;
+    d += 4;
+  }
+}
 
 # endif
--- CarpetLedLogic/CarpetLedLogic.ino
+++ CarpetLedLogic/CarpetLedLogic.ino
@@ -1,5 +1,5 @@
 
- #include <DmxSimple.h>
+#include <DmxSimple.h>
 #include <FastLED.h>
 #include "CRBGW.h"
 
@@ -11,42 +11,21 @@
 #define RGB_DATA_PIN 7 // 6
 #define RGBW_DATA_PIN 6 // 7
 
+// TODO: wrap all of these in a class that represents the whole carpet
 CRGB dmxleds[NUM_DMX_LEDS];
 CRGB rgbleds[NUM_RGB_LEDS];
-
-static void convertCRGBW2CRGB( CRGBW *src, CRGB *dst ) {
-  dst[0] = CRGB(src[0].r, src[0].g, src[0].b); //(red led 1, green on led 1, blue led 1)
-  dst[1] = CRGB(src[1].g, src[0].w, src[1].r); //(green led 2, white on led 1, red led 2)
-  dst[2] = CRGB(src[1].w, src[1].b, src[2].g); //(white led 2,blue led 2, green led 3)
-  dst[3] = CRGB(src[2].b, src[2].r, src[2].w); //(blue led 3, red led 3, white led 3)
-}
-
-static void covertCRGBWArrayToCRGBArray( CRGBW *src, CRGB *dst ) {
-  int s, d;
-  // we want to cut out if the entire array has been converted, or if we're out of leds
-  while ( s < NUM_RGBW_LEDS && d < NUM_THEORETICAL_RGBW_LEDS ) {
-    convertCRGBW2CRGB( src + s, dst + d );
-    s += 3;
-    d += 4;
-  }
-}
 
 struct CRGBW rgbwleds[NUM_RGBW_LEDS];
 CRGB convertedrgbwleds[NUM_THEORETICAL_RGBW_LEDS];
 
-void setup() {
-  // FastLED.addLeds<DMXSIMPLE, DMX_DATA_PIN, RGB>(dmxleds, NUM_DMX_LEDS);
-  // FastLED.addLeds<NEOPIXEL, RGB_DATA_PIN>(rgbleds, NUM_RGB_LEDS);
-
-  // this needs to be passed the raw led array, we'll handle to conversion elsewhere
-  FastLED.addLeds<NEOPIXEL, RGBW_DATA_PIN>( convertedrgbwleds, NUM_THEORETICAL_RGBW_LEDS);
+void robeDemoSetup() {
   for ( int i = 0; i < NUM_RGBW_LEDS; ++i ) {
     rgbwleds[i] = CRGB::LightSteelBlue;
   }
   FastLED.show();
 }
 
-void loop() {
+void robeDemo() {
   static int loc = 0;
   static int end = NUM_RGBW_LEDS;
   static int flip = 0;
@@ -72,7 +51,20 @@ void loop() {
   if ( loc == 0 ) {
     --end;
   }
-  covertCRGBWArrayToCRGBArray( rgbwleds, convertedrgbwleds );
+  covertCRGBWArrayToCRGBArray( rgbwleds, convertedrgbwleds, NUM_RGBW_LEDS, NUM_THEORETICAL_RGBW_LEDS );
   FastLED.show(); 
   delay(50);
+}
+
+void setup() {
+  // FastLED.addLeds<DMXSIMPLE, DMX_DATA_PIN, RGB>(dmxleds, NUM_DMX_LEDS);
+  // FastLED.addLeds<NEOPIXEL, RGB_DATA_PIN>(rgbleds, NUM_RGB_LEDS);
+
+  // this needs to be passed the raw led array, we'll handle to conversion elsewhere
+  FastLED.addLeds<NEOPIXEL, RGBW_DATA_PIN>( convertedrgbwleds, NUM_THEORETICAL_RGBW_LEDS);
+  robeDemoSetup();
+}
+
+void loop() {
+   robeDemo();
 }
