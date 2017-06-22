23f1296b3088c6fe6caf446c67ce6513141b519f
Anders Linn <anderslinn@gmail.com>
2017-06-21 21:44:37 -0700

    Added AliasDemo and ColorDemo
    Created two new demo modes to show off the lights and make debugging
    easier. AliasDemo is playing around with fractional interval changes;
    we'll want to get good at this as it makes light movement look really
    cool. ColorDemo is experimenting with having palettes-of-palettes; we
    eventually want to have a huge collection of dope palettes that we can
    blend between.
    
    Added namespaces around all the demo modes, including the legacy dmx
    modes. Cleaned up a few small things (made the SINELUT array look sane).

--- CarpetLedLogic/CarpetLedLogic.ino
+++ CarpetLedLogic/CarpetLedLogic.ino
@@ -20,21 +20,25 @@
 #include "MagicCarpet.h"
 #include "LegacyRoutines.h"
 
-#define NUM_RGBW_LEDS 10
-#define NUM_THEORETICAL_RGBW_LEDS ( NUM_RGBW_LEDS + ( NUM_RGBW_LEDS / 3 ) )
+/* Rope demo
+ *
+ * Basic shooting lights demo to test the full carpet rope led setup.
+ */
 
-static MagicCarpet * carpet;
+namespace RopeDemo {
+
+MagicCarpet * carpet;
 
 void ropeDemoSetup() {
    carpet = theMagicCarpet();
    carpet->setup();
-   for ( int i = 0; i < NUM_RGBW_LEDS; ++i ) {
+   for ( int i = 0; i < NUM_NEO_LEDS; ++i ) {
      carpet->ropeLeds[i] = CRGB::Red;
      carpet->ropeLeds[i].w = 0x0;
    }
    carpet->show();
    FastLED.delay( 1000 );
-   for ( int i = 0; i < NUM_RGBW_LEDS; ++i ) {
+   for ( int i = 0; i < NUM_NEO_LEDS; ++i ) {
      carpet->ropeLeds[i] = CRGB::Black;
    }
    carpet->show();
@@ -43,12 +47,12 @@ void ropeDemoSetup() {
 
 void ropeDemo() {
   static int loc = 0;
-  static int end = NUM_RGBW_LEDS;
+  static int end = NUM_NEO_LEDS;
   static int flip = 0;
   static CRGB clr( random8() % 255, random8() % 255, random8() % 255 );
   static CRGB oldclr( random8() % 255, random8() % 255, random8() % 255 );
   if ( end == 0 ) {
-    end = NUM_RGBW_LEDS;
+    end = NUM_NEO_LEDS;
     if ( flip == 0 ) {
       flip = -1;
     } else {
@@ -58,7 +62,7 @@ void ropeDemo() {
     clr = CRGB( random8() % 255, random8() % 255, random8() % 255 );
     return;
   }
-  for ( int i = 0; i < NUM_RGBW_LEDS; ++i ) {
+  for ( int i = 0; i < NUM_NEO_LEDS; ++i ) {
     if ( i == loc ) {
       carpet->ropeLeds[i] = clr;
       break;
@@ -72,15 +76,159 @@ void ropeDemo() {
     --end;
   }
   carpet->show();
-  FastLED.delay( 10 );
+  FastLED.delay( 1000 );
 }
 
+} // end namespace RopeDemo
+
+/* Alias demo
+ *
+ * Show off how using fractional interval changes makes moving lights look doper.
+ */
+
+namespace AliasDemo {
+
+#define NUM_ALIAS_DEMO_LEDS 10
+#define ALIAS_DEMO_DATA_PIN 52
+
+// pointer to alias demo leds
+CRGBW * leds;
+
+static CRGBW * aliasDemoLeds() {
+   CRGBW * leds = new CRGBW[ NUM_ALIAS_DEMO_LEDS ];
+   return leds;
+}
+
+void aliasDemoSetup() {
+   leds = aliasDemoLeds();
+   FastLED.addLeds<NEOPIXEL, ALIAS_DEMO_DATA_PIN>( leds, NUM_ALIAS_DEMO_LEDS );
+   fill_solid( leds, NUM_ALIAS_DEMO_LEDS, CRGB::Red );
+   FastLED.show();
+   FastLED.delay( 1000 );
+   fill_solid( leds, NUM_ALIAS_DEMO_LEDS, CRGB::Black );
+   FastLED.show();
+   FastLED.delay( 1000 );
+}
+
+void aliasDemoLoop() {
+   static int idx = 0;
+   static bool up = true;
+   static long start = millis();
+   static long tock = 0;
+   long tick = ( millis() - start ) % 1000;
+   uint8_t delta = 1000 - tick;
+   if ( tick < tock ) {
+      if ( up ) {
+         if ( ++idx == NUM_NEO_LEDS ) {
+            up = false;
+         }
+      } else if ( --idx == 0 ) {
+         up = true;
+      }
+   }
+   tock = tick;
+   delta /= 4;
+   for ( int i = 0; i < NUM_NEO_LEDS; ++i ) {
+      if ( i == idx - 1 ) {
+         leds[i] = CHSV( 150, 255, 255 - delta );
+      } else if ( i == idx ) {
+         leds[i] = CHSV( 150, 255, 255 );
+      } else if ( i == idx + 1 ) {
+         leds[i] = CHSV( 150, 255, 255 - delta );
+      } else {
+         leds[i] = CRGB::Black;
+      }
+   }
+   FastLED.show();
+}
+
+} // end namespace AliasDemo
+
+/* Color demo
+ *
+ * Playground for messing around with FastLED color palettes using a neopixel shield
+ */
+
+namespace ColorDemo {
+
+#define NUM_COLOR_DEMO_LEDS 10
+#define COLOR_DEMO_DATA_PIN 52
+
+// pointer to rgb led shield
+CRGB * leds;
+
+static CRGB * colorDemoLeds() {
+   CRGB * leds = new CRGB[ NUM_COLOR_DEMO_LEDS ];
+   return leds;
+}
+
+void colorDemoSetup() {
+   leds = colorDemoLeds();
+   FastLED.addLeds<NEOPIXEL, COLOR_DEMO_DATA_PIN>( leds, NUM_COLOR_DEMO_LEDS );
+   for ( int i = 0; i < NUM_COLOR_DEMO_LEDS; ++i ) {
+      leds[ i ] = CRGB::Red;
+   }
+   FastLED.setBrightness( 55 );
+   FastLED.setDither( true );
+   FastLED.setCorrection( TypicalLEDStrip );
+   FastLED.show();
+   FastLED.delay( 1000 );
+}
+
+void colorDemoLoop() {
+   static uint8_t location = 0;
+   static uint8_t phase = 0;
+   static bool phaseDirection = true;
+   static uint8_t stepSize = 255 / NUM_COLOR_DEMO_LEDS;
+   // TODO: set the color palette based on analog input
+   static long drawTock = millis();
+   static long locationTock = millis();
+   long tick = millis();
+   if ( tick > drawTock + 40 ) {
+      CRGBPalette16 skyPalette( CRGB( 0, 255, 255 ), CRGB( 0x0E1958 ), CRGB( 0, 204, 0 ) );
+      CRGBPalette16 groundPalette( CRGB( 255, 204, 102 ), CRGB( 0x423251 ), CRGB( 153, 0, 51 ) );
+      CRGB sky = ColorFromPalette( skyPalette, phase );
+      CRGB ground = ColorFromPalette( groundPalette, phase );
+      CRGBPalette16 palette( sky, ground, sky );
+      uint8_t colorIndex = 0;
+      for ( uint8_t i = 0; i < NUM_COLOR_DEMO_LEDS; ++i ) {
+         // ColorFromPalette wraps around in some circumstances, gotta fix that
+         uint8_t idx = ( location + i ) % NUM_COLOR_DEMO_LEDS;
+         auto clr = ColorFromPalette( palette, colorIndex );
+         leds[ idx ] = gammaCorrect( clr );
+         // update this to use a sine function for faster transfers
+         colorIndex += stepSize;
+      }
+      if ( phaseDirection ) {
+         if ( ++phase == 240 ) {
+            phaseDirection = false;
+         }
+      } else {
+         if ( --phase == 30 ) {
+            phaseDirection = true;
+         }
+      }
+      drawTock = tick;
+   }
+   if ( tick > locationTock + 180 ) {
+      ++location;
+      locationTock = tick;
+   }
+   FastLED.show();
+}
+
+} // end namespace ColorDemo
+
 void setup() {
-   // dmxSetup();
-   ropeDemoSetup();
+   // LegacyRoutines::dmxSetup();
+   // RopeDemo::ropeDemoSetup();
+   // ColorDemo::colorDemoSetup();
+   AliasDemo::aliasDemoSetup();
 }
 
 void loop() {
-   // dmxLoop();
-   ropeDemo();
+   // LegacyRoutines::dmxLoop();
+   // RopeDemo::ropeDemo();
+   // ColorDemo::colorDemoLoop();
+   AliasDemo::aliasDemoLoop();
 }
