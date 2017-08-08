284827c185b826d82898f2f54a1dd98536bd9cbb
Anders Linn <anderslinn@gmail.com>
2017-08-07 19:41:04 -0700

    Added Pascal's dmxtest4.ino
    This has his latest version of the dmx-update code. Needs to be ported
    over to a proper .cpp file, but the hard work has been done here.

---
+++ dmxtest4/dmxtest4.ino
@@ -0,0 +1,77 @@
+#include "sam.h"
+#include <FastLED.h>
+
+// Units are in timer ticks. 
+#define DMX_BRKTIME       (100)       // Time to hold serial in break before mark
+#define DMX_MARKTIME      (25)         // Time to hold serial in mark after break
+#define DMX_BAUDRATE      (250000)     // DMX baurate
+#define DMX_FRAMING       (SERIAL_8N2) // Serial framing configuraiton
+#define DMX_UART          (USART0)     // Due USART device (must be a USART not a UART)
+#define DMX_SERIAL        (Serial1)    // Arduinoish serial object associated with above USART
+
+/* DMX state */
+bool dmx_inited = false;    // To prevent double-init
+uint8_t *_dmx_buf;          // Private buffer the DMA to serial
+size_t dmx_buflen;          // Buffer length
+
+/* Init and start the DMX system */
+void dmx_init(size_t buflen) {
+   // Don't double-init
+   if(dmx_inited == true) {
+     return;
+   }
+
+   dmx_buflen = buflen;
+   _dmx_buf = (uint8_t *)malloc(buflen+1);
+   memset(_dmx_buf, 0, buflen+1);
+
+   // Init serial port
+   DMX_SERIAL.begin(DMX_BAUDRATE, DMX_FRAMING);
+}
+
+void dmx_send(uint8_t *buf)
+{
+  _dmx_buf[0] = 0;
+  memcpy(_dmx_buf+1, buf, dmx_buflen);
+  DMX_UART->US_CR |= US_CR_STTBRK;   // Start break
+  delayMicroseconds(DMX_BRKTIME);
+  DMX_UART->US_CR |= US_CR_STPBRK;  // Stop break signal
+  delayMicroseconds(DMX_MARKTIME);
+  DMX_SERIAL.write(_dmx_buf, dmx_buflen);
+  return;
+}
+
+CRGB rope[ 1024 ];
+CRGB dmx[ 18 ];
+void setup() {
+  pinMode( 32, OUTPUT );
+  pinMode( 37, OUTPUT );
+  digitalWrite(32, LOW); 
+  digitalWrite(37, LOW); 
+  FastLED.addLeds<WS2811_PORTD, 8>( rope, 128 );
+  dmx_init(18*sizeof(CRGB));
+}
+
+void loop() {
+  for (int i = 0; i < 1024; ++i ) {
+     rope[ i ].r = random(255);
+     rope[ i ].b = random(255);
+     rope[ i ].g = random(255);
+  }
+  
+  for(int i=0;i<18;i++) {
+      dmx[i].r = random(255);
+      dmx[i].b = random(255);
+      dmx[i].g = random(255);
+  }
+   
+   digitalWrite(32, HIGH);
+   dmx_send((void *)dmx);
+   digitalWrite(32, LOW);
+
+   digitalWrite(37, HIGH);
+   FastLED.show();
+   digitalWrite(37, LOW);
+
+   delayMicroseconds( 100000 );
+}
