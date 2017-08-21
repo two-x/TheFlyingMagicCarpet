43b772081498a1dcdbae563561f1539c9502515c
Anders Linn <anderslinn@gmail.com>
2017-08-20 17:58:09 -0700

    Add test code in main loop
    I'm kind of in the middle of using this stuff, but it works and i want
    to get everything pushed up to remote master so I'm going ahead and
    commiting it for now. I'll pull it out when I write the main control
    loop stuff (after the sound).

--- CarpetLedLogic/CarpetLedLogic.ino
+++ CarpetLedLogic/CarpetLedLogic.ino
@@ -33,6 +33,51 @@ void loop() {
 
    clock = millis();
 
+   AudioBoard::pollFrequencies( clock );
+
+   if ( true ) {
+      Serial.println("millis");
+      Serial.println( clock );
+      Serial.println("output");
+      Serial.println( AudioBoard::bin_low );
+      Serial.println( AudioBoard::bin_mid );
+      Serial.println( AudioBoard::bin_high );
+      Serial.println("input");
+      Serial.println(AudioBoard::Frequencies_Mono[0]);
+      Serial.println(AudioBoard::Frequencies_Mono[1]);
+      Serial.println(AudioBoard::Frequencies_Mono[2]);
+      Serial.println(AudioBoard::Frequencies_Mono[3]);
+      Serial.println(AudioBoard::Frequencies_Mono[4]);
+      Serial.println(AudioBoard::Frequencies_Mono[5]);
+      Serial.println(AudioBoard::Frequencies_Mono[6]);
+   }
+
+   static const CRGBPalette16 clr( CRGB::Red, CRGB::Black );
+
+   // for ( int j = 0; j < NUM_MEGABAR_LEDS; ++j ) {
+   //    carpet->megabarLeds[ j ] = CRGB::Green;
+   // }
+   // if ( carpet->button->isDown() ) {
+   //    for ( int j = 0; j < NUM_MEGABAR_LEDS; ++j ) {
+   //       carpet->megabarLeds[ j ] = CRGB::Red;
+   //    }
+   // } else {
+   //    uint16_t val = scaleTo255( carpet->pot->read(), 1024, 0 );
+   //    for ( int j = 0; j < NUM_MEGABAR_LEDS; ++j ) {
+   //       carpet->megabarLeds[ j ] = CHSV( val, 255, 255 );
+   //    }
+   // }
+   for ( int j = 0; j < 7; ++j ) {
+      carpet->ropeLeds[ NEO1_OFFSET - 1 - j ] = ColorFromPalette( clr, scaleTo255( AudioBoard::Frequencies_Mono[j], 1024, 0 ) );
+   }
+   carpet->ropeLeds[ NEO1_OFFSET - 8 ] = ColorFromPalette( clr, AudioBoard::getLow() );
+   carpet->ropeLeds[ NEO1_OFFSET - 9 ] = ColorFromPalette( clr, AudioBoard::getMid() );
+   carpet->ropeLeds[ NEO1_OFFSET - 10 ] = ColorFromPalette( clr, AudioBoard::getHigh() );
+
+   // for ( int j = 0; j < NEO1_OFFSET; ++j ) {
+   //    carpet->ropeLeds[ j ] = CHSV( scaleTo255( carpet->pot->read(), 1024, 0 ), 255, 255 );
+   // }
+
    // read encoder
    // based on diff, switch between existing light shows
 
