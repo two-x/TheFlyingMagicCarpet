f440d7dbe01439e95a73be9faf485d292b0a0f6e
two-x <41216347+two-x@users.noreply.github.com>
2023-06-16 19:25:19 -0700

    Major update (#20)
    Moved pressure, pot, tach, and speedo parameters from integers to doubles, in human understandable units instead of us or adc counts.
    Multiple display improvements.
    Motor/PID logic simplified structure. No more local version of PID target, it is kept within the SPID class.
    Added a loop timer to learn how much time each section of code takes to run
    HotRC radio detection feature works and will cause a panic stop if the batteries die, etc while driving. (commented out now for development).
    Temperature sensors implemented but not yet successfully tested.
    Improvements to mode logic and mode-specific behaviors.
    
    * making new branch
    
    * starting at master clone
    
    * Merged changes from broken code #1
    
    * Fixed crashing. Now must remerge most changes
    Issue was with syspower delay
    
    * Fixed some debug output stuff
    
    * Big batch of changes is in, still stable.
    
    * Another big batch of changes merged, still stable
    
    * About 2/3 of my changes are in now and not
    crashing. However, the dataset pages aren't
    correctly erasing old variable names amd units
    
    * Moved in more changes, fixed display erase bugs,
    runs very stable. Just 1 or 2 more leaps to go.
    
    * I am all caught up. All features I had implemented
    are present, and along the way I fixed the bug
    causing it to crash. Not 100% sure which thing
    fixed it, but it has to do with the I2C missing
    signals when the code does too much in one loop.
    Like, if a loop takes over 50 ms things get risky.

--- CANTroller2/src/classes.h
+++ CANTroller2/src/classes.h
@@ -1,12 +1,10 @@
 #ifndef CLASSES_H
 #define CLASSES_H
-#undef min
-#undef max
 
 #include <stdio.h>
 #include <iostream>
 #include "Arduino.h"
-
+// #include "xtensa/core-macros.h"  // access to ccount register for esp32 timing ticks
 using namespace std;
 
 int32_t mycros(void) {  // This is "my" micros() function that returns signed int32
@@ -14,31 +12,62 @@ int32_t mycros(void) {  // This is "my" micros() function that returns signed in
     return (int32_t)(temp &= 0x7fffffff);  // Note this overflows every 35 min due to being only 31 bits. 
 }
 
+// int32_t nanos(void) {  // Uses CCount register of ESP
+//     uint32_t ccount = XTHAL_GET_CCOUNT();
+//     return (int32_t)(temp &= 0x7fffffff);
+// }
+
 // Timer
 // For controlling event timing. Communicates in signed int32 values but uses uint32 under the hood to
 // last 72 minutes between overflows. However you can only time for 36 minutes max.
 // Note: experimenting with use of micros() instead of mycros(), check for errors on overflow and possibly revert
 class Timer {
+  protected:
     volatile uint32_t start_us;  // start time in us
     int32_t timeout_us = 0;  // in us
-    public:
-        Timer(void) { start_us = micros(); };
-        Timer(int32_t arg1) { set(arg1); };
+  public:
+    Timer(void) { start_us = micros(); };
+    Timer(int32_t arg1) { set(arg1); };
 
-        void reset()  { start_us = micros(); }
-        bool expired()  { return (abs((int32_t)(micros() - start_us)) > timeout_us); }
-        int32_t elapsed()  { return abs((int32_t)(micros() - start_us)); }
-        int32_t timeout()  { return (int32_t)timeout_us; }
-        void set(int32_t arg1)  {
-            timeout_us = arg1;
-            start_us = micros();
-        }
-        int32_t remain()  { 
-            uint32_t temp = abs((int32_t)(micros() - start_us));
-            return (timeout_us - (int32_t)temp);
-        }
+    void reset()  { start_us = micros(); }
+    bool expired()  { return (abs((int32_t)(micros() - start_us)) > timeout_us); }
+    int32_t elapsed()  { return abs((int32_t)(micros() - start_us)); }
+    int32_t timeout()  { return (int32_t)timeout_us; }
+    void set(int32_t arg1)  {
+        timeout_us = arg1;
+        start_us = micros();
+    }
+    int32_t remain()  { 
+        uint32_t temp = abs((int32_t)(micros() - start_us));
+        return (timeout_us - (int32_t)temp);
+    }
 };
 
+class TimerESP {
+  protected:
+    volatile uint32_t start_us;  // start time in us
+    int32_t timeout_us = 0;  // in us
+    bool preciseESP = false;
+  public:
+    TimerESP(void) { start_us = micros(); }
+    TimerESP(bool precise) {
+        preciseESP = precise;
+    }
+    TimerESP(int32_t arg1) { set(arg1); }
+
+    void reset()  { start_us = micros(); }
+    bool expired()  { return (abs((int32_t)(micros() - start_us)) > timeout_us); }
+    int32_t elapsed()  { return abs((int32_t)(micros() - start_us)); }
+    int32_t timeout()  { return (int32_t)timeout_us; }
+    void set(int32_t arg1)  {
+        timeout_us = arg1;
+        start_us = micros();
+    }
+    int32_t remain()  { 
+        uint32_t temp = abs((int32_t)(micros() - start_us));
+        return (timeout_us - (int32_t)temp);
+    }
+};
 // class Variable {  // A wrapper for all veriables of consequence in our code.
 //     public:
 //         Variable(int32_t arg1) { int32_t value = arg1; }
@@ -388,4 +417,4 @@ class Param {
 // class Settings {
 // };
 
-#endif
+#endif  // CLASSES_H
