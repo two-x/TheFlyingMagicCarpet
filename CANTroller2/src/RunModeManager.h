e13900da7d5b3c148d779e25603c99cddfc39735
soren <soren@two-x.com>
2023-10-07 21:04:05 -0700

    Fixed a bug in cruise logic. Cruise needs to be tested thoroughly just in case

--- CANTroller2/src/RunModeManager.h
+++ CANTroller2/src/RunModeManager.h
@@ -188,7 +188,7 @@ private:
             // cruiseQPID.SetCenter (tach_filt_rpm);
             gestureFlyTimer.reset();  // reset gesture timer
             cruise_trigger_released = false;  // in case trigger is being pulled as cruise mode is entered, the ability to adjust is only unlocked after the trigger is subsequently released to the center
-            gas_pulse_cruise_us = gas_pulse_out_us;  // This variable stores the setpoint, either of throttle angle (if cruise_fixed throttle=true) or rpm target (if false)
+            gas_pulse_cruise_us = gas_pulse_out_us;  //  if cruise_fixed throttle is true, this variable stores the setpoint of throttle angle
             gas_pulse_adjustpoint_us = gas_pulse_cruise_us;  // Pull of trigger away from center in either direction starts a setpoint adjustment, scaled from *your current setpoint* (not from the center value) to the relevant min or max extreme 
             cruise_ctrl_extent_adc = (float)ctrl_lims_adc[ctrl][VERT][CENT];  // After an adjustment, need this to prevent setpoint from following the trigger back to center as you release it
             cruise_adjusting = false;
@@ -209,9 +209,9 @@ private:
             }
         }
         else if (ctrl_pos_adc[VERT][FILT] < ctrl_db_adc[VERT][BOT]) {  // When joystick vert below center, decrease the speed target proportional to how far off center
-            if (!cruise_speed_lowerable) updateMode(FLY);  // Then any trigger braking activity cancels cruise mode
-            else if (cruise_trigger_released && ctrl_pos_adc[VERT][FILT] <= cruise_ctrl_extent_adc) {
-                if (cruise_fixed_throttle) {
+            if (cruise_trigger_released && ctrl_pos_adc[VERT][FILT] <= cruise_ctrl_extent_adc) {
+                if (!cruise_speed_lowerable) updateMode(FLY);  // Then any trigger braking activity cancels cruise mode
+                else if (cruise_fixed_throttle) {
                     if (!cruise_adjusting) gas_pulse_adjustpoint_us = gas_pulse_cruise_us;  // When beginning adjustment, save current throttle pulse value to use as adjustment low endpoint
                     gas_pulse_cruise_us = map ((float)ctrl_pos_adc[VERT][FILT], (float)ctrl_db_adc[VERT][BOT], (float)ctrl_lims_adc[ctrl][VERT][MIN], gas_pulse_adjustpoint_us, gas_pulse_ccw_closed_us);
                 }
