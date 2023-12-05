5e6c9563dd827cda6867ceae7560974f8e9e7df1
soren <soren@two-x.com>
2023-12-04 16:00:54 -0800

    Now can use capacitive or resistive based on a define in touch.h Capacitive hasn't been tested till the new screens arrive

--- CANTroller2/src/tftsetup.h
+++ CANTroller2/src/tftsetup.h
@@ -84,4 +84,4 @@
 // so changing it here has no effect
 // #define SUPPORT_TRANSACTIONS
 
-#define TFT_INVERSION_ON  // #define TFT_INVERSION_ON
+#define TFT_INVERSION_OFF  // #define TFT_INVERSION_ON
--- CANTroller2/src/touch.h
+++ CANTroller2/src/touch.h
@@ -1,17 +1,24 @@
 
 #pragma once
-// #include <XPT2046_Touchscreen.h>
-#include <Adafruit_FT6206.h>
+#undef CAPTOUCH
+#ifdef CAPTOUCH
+  #include <Adafruit_FT6206.h>
+  #define SENSITIVITY 40
+#else
+  #include <XPT2046_Touchscreen.h>
+#endif
 #define touch_cell_v_pix 48  // When touchscreen gridded as buttons, height of each button
 #define touch_cell_h_pix 53  // When touchscreen gridded as buttons, width of each button
 #define touch_margin_h_pix 1  // On horizontal axis, we need an extra margin along both sides button sizes to fill the screen
 #define touch_reticle_offset 50  // Distance of center of each reticle to nearest screen edge
 #define disp_tuning_lines 11  // Lines of dynamic variables/values in dataset pages 
-#define SENSITIVITY 40
 class TouchScreen {
 private:
-    // XPT2046_Touchscreen _ts;  // 3.5in resistive touch panel on tft lcd
-    Adafruit_FT6206 _ts;
+    #ifdef CAPTOUCH
+      Adafruit_FT6206 _ts;
+    #else
+      XPT2046_Touchscreen _ts;  // 3.5in resistive touch panel on tft lcd
+    #endif
     bool touch_longpress_valid = true;
     bool touch_now_touched = false;
     int tedit_exponent = 0;
@@ -40,15 +47,22 @@ private:
 
     TS_Point touchpoint;
 public:
-    // TouchScreen(uint8_t csPin, uint8_t irqPin = 255) : _ts(csPin, irqPin) {}
+    #ifdef CAPTOUCH
     TouchScreen(uint8_t csPin, uint8_t irqPin = 255) : _ts() {}
+    #else
+    TouchScreen(uint8_t csPin, uint8_t irqPin = 255) : _ts(csPin, irqPin) {}
+    #endif
     int idelta = 0;
     void setup(int width, int height) {
         disp_width = width;
         disp_height = height;
         printf("touchscreen..\n");
-        _ts.begin(SENSITIVITY, &Wire);
-        // _ts.setRotation(1); do we need to rotate?
+        #ifdef CAPTOUCH
+          _ts.begin(SENSITIVITY, &Wire);
+        #else
+          _ts.begin();
+        #endif
+        _ts.setRotation(1);  // do we need to rotate?
     }
 
     bool touched() {
